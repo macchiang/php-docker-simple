2b2732cb3b4832bd574e5182f0a904910cbe165c
Mac Chiang <macchiang@gmail.com>
2021-11-12 10:46:38 +0800

    add create db/table

--- src/index.php
+++ src/index.php
@@ -1,22 +1,44 @@
 <?php
 
-echo "Hello from the docker yooooo container";
+echo "Hello from the docker yooooo container<br />";
 
-$mysqli = new mysqli("db", "root", "example", "company1");
+$conn = new mysqli("db", "root", "example");
+
+if ($conn->query("CREATE DATABASE IF NOT EXISTS company1")) {
+    printf("Database company1 created successfully.<br />");
+ }
+ if ($conn->errno) {
+    printf("Could not create database: %s<br />", $conn->error);
+ }
+
+$mysqli = mysqli_select_db ($conn, "company1");
+
+
+$sql = "CREATE TABLE IF NOT EXISTS users( ".
+"name VARCHAR(100), ".
+"fav_color VARCHAR(100) ".
+"); ";
+
+if ($conn->query($sql)) {
+    printf("Table users created successfully.<br />");
+}
+if ($conn->errno) {
+    printf("Could not create table: %s<br />", $conn->error);
+}
 
 $sql = "INSERT INTO users (name, fav_color) VALUES('Lil Sneazy', 'Yellow')";
-$result = $mysqli->query($sql);
+$result = $conn->query($sql);
 $sql = "INSERT INTO users (name, fav_color) VALUES('Nick Jonas', 'Brown')";
-$result = $mysqli->query($sql);
+$result = $conn->query($sql);
 $sql = "INSERT INTO users (name, fav_color) VALUES('Maroon 5', 'Maroon')";
-$result = $mysqli->query($sql);
+$result = $conn->query($sql);
 $sql = "INSERT INTO users (name, fav_color) VALUES('Tommy Baker', '043A2B')";
-$result = $mysqli->query($sql);
+$result = $conn->query($sql);
 
 
 $sql = 'SELECT * FROM users';
 
-if ($result = $mysqli->query($sql)) {
+if ($result = $conn->query($sql)) {
     while ($data = $result->fetch_object()) {
         $users[] = $data;
     }
